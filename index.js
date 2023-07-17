6b30b813c7209a9ec6c1fbfa39724fa3ab22a6a3
Mercyfulll <mercyful.buthelezi@gmail.com>
2023-07-17 09:04:14 +0200

    fourth commit

--- index.js
+++ index.js
@@ -3,52 +3,61 @@ document.addEventListener("alpine:init", () => {
         total: 0,
         count: 0,
         count2: 0,
-        count3: 0,
-        showSmall: true,
-        showMedium: true,
-        showLarge : true,
+        count3:0,
+        message:'',
+        amount: 0,
         smallAdd() {
             this.count += 29
         },
         smallMinus() {
             this.count -= 29
+            if (this.count < 0) {
+                this.count = 0;
+            }
         },
         mediumAdd() {
             this.count2 += 89
         },
         mediumMinus() {
             this.count2 -= 89
+            if (this.count2 < 0) {
+                this.count2 = 0;
+            }
         },
         largeAdd() {
             this.count3 += 130
         },
         largeMinus() {
             this.count3 -= 130
+            if (this.count3 < 0) {
+                this.count3 = 0;
+                openLarge == false
+            }
         },
-        cost() {
+        cost (){
             this.total = this.count + this.count2 + this.count3
         },
-        paid(amount) {
-            var message = ''
-            if (this.amount > this.total) {
-                message = "Enjoy your pizzas"
-            }
-            if (amount > this.total) {
+        paid() {
+            if (this.amount < this.total) {
+
+                this.message = "Sorry - that is not enough money!"
                 setTimeout(function(){
-                    message = "Sorry - that is not enough money!"
+                    this.message = ''
                   },3000);
-                
+            }
+            if (this.amount >= this.total) {
+                this.message = "Enjoy your pizzas"
+
+
             }
         },
         clearCart() {
-            if (this.count < 0) {
-                this.showSmall= false 
+           
+            if(this.count2 <= -1){
+                this.openMedium = false
             }
-            if(this.count2 < 0){
-                this.showMedium = false
-            }
-            if(this.count3 < 0)
-                this.showLarge = false
+            if(this.count3 <= -1)
+                this.openLarge = false
         }
         }))
 })
